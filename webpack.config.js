00f4385cfd0afd65ee4fc28f8986d86359e6f39d
Sergei Dolgov <dolgovs@gmail.com>
2019-11-16 13:19:06 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+var path = require('path');
+
+module.exports = {
+    mode: "development",
+
+    devtool: "source-map",
+
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"]
+    },
+
+    entry: {
+        bundle: './src/index.ts'
+    },
+
+    output: {
+        filename: '[name].js',
+        path: path.join(__dirname, '/deploy')
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.ts(x?)$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "ts-loader"
+                    }
+                ]
+            },
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                loader: "source-map-loader"
+            }
+        ]
+    },
+
+    devServer: {
+        contentBase: path.join(__dirname, 'static'),
+        compress: true,
+        port: 1234
+    }
+
+};
